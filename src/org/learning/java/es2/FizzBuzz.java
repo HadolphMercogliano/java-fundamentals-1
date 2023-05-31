760740d9a2b8ac615e9d74db26bad5732eb93935
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-31 15:51:19 +0200

    add FizzBuzz bonus

--- src/org/learning/java/es2/FizzBuzz.java
+++ src/org/learning/java/es2/FizzBuzz.java
@@ -1,8 +1,18 @@
 package org.learning.java.es2;
+import java.util.Scanner;
 
 public class FizzBuzz {
   public static void main(String[] args) {
-    for (int i = 1; i <= 100; i++) {
+    Scanner scanner = new Scanner(System.in);
+    int number;
+    do {
+      System.out.println("Quanti numeri (tra 1 e 1000) vuoi stampare?");
+      number = scanner.nextInt();
+      
+    }
+    while(number<0 || number>1000 );
+    
+    for (int i = 1; i <= number; i++) {
       
       if (i % 3 == 0 && i % 5 == 0) {
         System.out.print("FizzBuzz ");
