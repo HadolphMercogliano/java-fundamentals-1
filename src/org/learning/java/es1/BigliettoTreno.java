aed84c8477953fe180e1a43b68c4e96fad46b07e
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-05-31 14:26:37 +0200

    add input command

---
+++ src/org/learning/java/es1/BigliettoTreno.java
@@ -0,0 +1,21 @@
+package org.learning.java.es1;
+import java.util.Scanner;
+
+public class BigliettoTreno {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    double pricePerKm = 0.21;
+    
+    // chiedi quanti km si vuole percorrere e inserisci il valore nella variabile "km"
+    System.out.println("Quanti km vuoi percorrere? ");
+    int km = scanner.nextInt();
+    
+    // chiedi quanti anni ha il passeggero e inserisci il dato nella variabile "age"
+    System.out.println("Quanti anni hai? ");
+    int age = scanner.nextInt();
+    
+    
+    
+  }
+}
